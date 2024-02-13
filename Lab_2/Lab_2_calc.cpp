4bd651d20b965fb78314abfb8252aa7820fce897
brandon <brandonchanchez25@gmail.com>
2024-02-13 11:08:11 -0800

    OOP C++ second commit

---
+++ Lab_2/Lab_2_calc.cpp
@@ -0,0 +1,26 @@
+
+
+//Lab_2_calc.cpp
+
+/***
+ * calculation of two data types 
+  ***/
+
+
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    //numerical
+    int int_x = 1;
+    int int_y = 2;
+    double double_2 = 2.0;
+
+    cout << "int divided by int -> " << int_x / int_y << endl;
+    cout << "int divided by double -> " << int_x / double_2 << endl;
+
+    return 0;
+
+}
