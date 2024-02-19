cc9c1238ec921c9e3bec4146e5b3f31d337d755f
brandon <brandonchanchez25@gmail.com>
2024-02-18 22:04:38 -0800

    updated added Lab3 and the source code as well as images

--- Lab_3/Lab3_CF.cpp
+++ Lab_3/Lab3_CF.cpp
@@ -1,1 +1,26 @@
-#include
+#include <cstdlib>
+#include <iostream>
+
+/*This code is supposed to print the status of x when its
+less than 0 then increment it by 3 */
+
+int main(){
+    int x = 0;
+    int y = 5;
+
+    if (x < y){
+
+        printf("%i\n", x);
+        x +=  3; // Increment x by  3
+
+    }else if (x % 2 == 0){
+        printf("x is even: %i\n",x);
+
+    }else {
+        printf("x is odds: %i\n",x);
+
+    }
+
+    return 0;
+    
+}
