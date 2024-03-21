87db5b8d136e2b1b40030f76d8b46fd0c88eba40
Pan Dulce <pandulce@pop-os.localdomain>
2024-03-21 16:43:33 -0700

    finsished some parts of lab 7

---
+++ Lab_7/Lab_7.cpp
@@ -0,0 +1,92 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief writes sample_size number of strings 
+ * Example output: if n = 2 and arr = ["cat", "dog"]
+ * cat
+ * dog
+ * 
+ * @param filename
+ * @param arr
+ * @param sample_size 
+ */
+
+
+void writeToFile_str(string filename, string* arr, int sample_size) {
+    ofstream file(filename);
+
+    for(int i = 0; i < sample_size; i++) {
+        file << arr[i] << "\n";
+    }
+
+    file.close();
+    printf("Wrote to %s\n", filename.c_str());
+}
+
+
+void writeToFile_int(string filename, int* arr, int sample_size) {
+    ofstream file(filename);
+
+    for(int i = 0; i < sample_size; i++) {
+        file << arr[i] << "\n";
+    }
+
+    file.close();
+    printf("Wrote to %s\n", filename.c_str());
+}
+
+
+void writeToFile(string filename, char* arr, int sample_size) {
+    ofstream file(filename);
+
+    for(int i = 0; i < sample_size; i++) {
+        file << arr[i] << "\n";
+    }
+
+    file.close();
+    printf("Wrote to %s\n", filename.c_str());
+}
+
+
+void readTheFile(string filename2) {
+    ifstream file(filename2); 
+
+    if (!file.is_open()) { 
+        cerr << "Unable to open file " << filename2 << endl;
+        return;
+    }
+
+    string line;
+    while (getline(file, line)) { 
+        cout << line << endl;
+    }
+
+    file.close(); 
+}
+
+
+
+int main() {
+    string filename1 = "output_char.csv";
+    string filename2 = "output_str.csv";
+    string filename3 = "output_int.csv";
+
+    char arr_char[] = {'c', 'd'};
+    string arr_str[] = {"cat", "dog"}; 
+    int arr_int[] = {1, 0}; 
+    writeToFile(filename1, arr_char, 2); 
+    writeToFile_str(filename2, arr_str, 2); 
+    writeToFile_int(filename3, arr_int, 2);
+
+    // Read each file
+    readTheFile(filename1);
+    readTheFile(filename2);
+    readTheFile(filename3);
+
+    return 0;
+}
