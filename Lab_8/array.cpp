533f342300248e8ebe91ca0f3b75fc65ec40c171
Dulc3 <124217011+brandoz2255@users.noreply.github.com>
2024-04-11 00:01:52 -0700

    Fixed some issue with the code

--- Lab_8/array.cpp
+++ Lab_8/array.cpp
@@ -8,20 +8,20 @@ using namespace std;
  * @brief Prints the elements in the array and their memory locations 
  * 
  * @param - array of integers 
- * @param - the number og elements in the array 
+ * @param - the number of elements in the array 
  */
 
 void printMemArr(const int * arr, int size ){ // void printMemArry (int arr), int size)
 	printf("Array - Each int is worth %lu bytes\n", sizeof(arr[0]));
 	for (int i =0; i < size; i++){
-		printf("Value : %i at memory location: %p\n", arr[1], arr + 1); 
+		printf("Value : %i at memory location: %p\n", arr[i], arr + 1); 
 	}
 }
 
 
 void incArrBy10(int *  arr , int size) {// void intArrayBy10
 	for (int i = 0 ; i < size; i++) {
-		arr[1] += 10;
+		arr[i] += 10;
 	}
 }
 
@@ -32,11 +32,16 @@ int main(){
 	arr[i] = 100 +i; 	
 	}
 
-	printf("Before---------------\n");
+	printf("Before----------------\n");
 	printMemArr(arr,SIZE);
 	incArrBy10(arr,SIZE);
 	printf("After ----------------\n");
 	printMemArr(arr,SIZE);
+
+   
+
+
+
 	return 0;
 	
 }
---
+++ Lab_8/vector.cpp
@@ -0,0 +1,81 @@
+
+#include <iostream>
+#include <vector>
+//#include <cstdlib>
+
+using namespace std;
+/**
+* @brief prints the elements in the vector and their memory locations
+*
+* @param v - vector of integers
+*/
+
+
+
+void printMemVec(const vector<int>& vec) {
+    printf("Vector - Each int is worth %lu bytes\n", sizeof(vec[0]));
+    for (size_t i = 0; i < vec.size(); i++) {
+        printf("Value : %i at memory location: %p\n", vec[i], (void*)(&vec[i]));
+    }
+}
+
+
+
+
+/**
+* @brief increments all of the elements in v by 10
+*
+* @param v - address to a vector of integers
+*/
+
+void incVecBy10(vector<int>& vec) {
+    for (size_t i = 0; i < vec.size(); i++) {
+        vec[i] += 10;
+    }
+}
+
+
+int main(){
+    //Create a constant integer called SIZE that is of value 5
+    const int SIZE = 5;
+    //create a vector of integers called vec that can hold up to 5 elements
+    vector<int> vec(SIZE);
+
+    //use a for loop to populate vec with the values 100 to 104
+    for (int i = 0; i < SIZE; i++){
+        vec[i] = 100 + i;
+    }
+
+    printf("Before Increment------------\n");
+    //call printMemVec(...) on vec
+    printMemVec(vec);
+
+    //call incBy10(...) on vec
+
+
+    printf("After Increment-------------\n");
+    //call printMemVec(...) on vec again to view the changes
+    printMemVec(vec);
+
+
+    // remove last element of vec
+    vec.pop_back();
+
+
+    printf("After pop------------\n");
+
+    //call printmemVec(...) on vec again to view the changes 
+    printMemVec(vec);
+
+    // append 101 and 102 at end of vec
+    vec.push_back(101);
+    vec.push_back(102);
+
+    printf("After Push--------\n");
+
+    //cal printMemVec(...) on vec again to view the changes
+    
+    printMemVec(vec);
+    
+    return 0;
+}
